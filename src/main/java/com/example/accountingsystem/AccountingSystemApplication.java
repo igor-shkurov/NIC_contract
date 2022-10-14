9e301154dd765b2e13fd03a7ed97273c2c8e9950
Igor Shkurov <igor.shkurov.me@gmail.com>
2022-10-14 14:44:27 +0300

    Init commit

---
+++ src/main/java/com/example/accountingsystem/AccountingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.accountingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/accountingsystem/AccountingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accountingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
