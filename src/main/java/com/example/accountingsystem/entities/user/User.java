ef198f21f3706af21c68614df589bcc9cac5e503
Igor Shkurov <igor.shkurov.me@gmail.com>
2022-11-23 18:26:06 +0300

    Changed the order of properties in user json/turned off the security module

--- src/main/java/com/example/accountingsystem/entities/user/User.java
+++ src/main/java/com/example/accountingsystem/entities/user/User.java
@@ -4,9 +4,14 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import com.example.accountingsystem.security.SecurityConfiguration;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
@@ -16,6 +21,7 @@ import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.util.Collection;
 
+@JsonPropertyOrder({"id", "fio", "username", "password"})
 @Entity
 @Table
 public class User implements UserDetails {
@@ -80,40 +86,48 @@ public class User implements UserDetails {
         this.FIO = FIO;
     }
 
+    @JsonIgnore
     public LocalDateTime getExpirationDate() {
         return expirationDate;
     }
 
+    @JsonIgnore
     public void setExpirationDate(LocalDateTime expirationDate) {
         this.expirationDate = expirationDate;
     }
 
     /* todo: Подумать как булевские методы должны работать (осмысленно)  */
+    @JsonIgnore
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
 
+    @JsonIgnore
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
 
+    @JsonIgnore
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
 
+    @JsonIgnore
     @Override
     public boolean isEnabled() {
         return true;
     }
 
+    @JsonIgnore
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return Collections.singleton(new SimpleGrantedAuthority("ROLE_" + getRole().toString()));
     }
 
+    @JsonIgnore
     public Role getRole() {
         return role;
     }
